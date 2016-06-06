9f2447d580e5f7d2744290caeb9c0cda0d82d77b
sarasss08 <saraswathy1120@gmail.com>
2016-06-06 23:43:28 +0530

    Create mypgm.c

---
+++ mypgm.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+{
+	printf("\n Number is positive");
+}	
+else if(n<0)
+{
+	printf("\n Number is negative");
+}
+else
+	{
+		printf("\n Number is zero");
+	}// your code goes here
+	return 0;
+}
