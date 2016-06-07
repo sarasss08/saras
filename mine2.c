c0cdc97698e1d7058be98182935c961fc34e97e4
sarasss08 <saraswathy1120@gmail.com>
2016-06-07 12:14:52 +0530

    Create mine2.c

---
+++ mine2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("\n Number is even");
+	}
+	else
+	{
+		printf("\n NUmber is odd");
+	}
+	// your code goes here
+	return 0;
+}
