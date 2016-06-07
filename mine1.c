675e1146741854ab16c26e8cb3b3093ef9874233
sarasss08 <saraswathy1120@gmail.com>
2016-06-07 12:12:27 +0530

    Create mine1.c

---
+++ mine1.c
@@ -0,0 +1,21 @@
+    #include <stdio.h>
+     
+    int main(void) {
+    	int n;
+    	scanf("%d",&n);
+    	if(n>0)
+    	{
+    		printf("\n Number is positive");
+    	}
+    	else if(n<0)
+    	{
+    		printf("\n Number is NEgative");
+    	}
+    	else
+    	{
+    		printf("\n NUmber is Zero");
+    	}
+    	// your code goes here
+    	return 0;
+    }
+     
